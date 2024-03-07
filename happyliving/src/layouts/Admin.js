3180339147ab6b0e0eb62cc733ecd876e6bce53b
Sufiyan Shaikh <sufiyan.m.shaikh248@gmail.com>
2024-03-07 21:20:55 +0530

    Added package-name

--- happyliving/src/layouts/Admin.js
+++ happyliving/src/layouts/Admin.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 // components
 
-// import AdminNavbar from "./../components/Navbars/AdminNavbar.js";
+import AdminNavbar from "./../components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
@@ -79,7 +79,7 @@ export default function Admin() {
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
-        {/* <AdminNavbar /> */}
+        <AdminNavbar />
         {/* Header */}
         <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
