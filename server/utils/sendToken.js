e5498fd65eff8175407dde715f9e1a98ab9e7a4b
Sufiyan Shaikh <sufiyan.m.shaikh248@gmail.com>
2024-03-08 23:10:38 +0530

    Trying to solve not logging problem

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -4,8 +4,11 @@ export const sendToken = (res, user, message, statuscode = 200)=>{
     const options = {
         expires: new Date(Date.now() + 1000*60*60*24*15),
         httpOnly: true,
-        secure: true, 
-        sameSite: "none",
+        // secure: true, 
+        secure: process.env.NODE_ENV === 'PROD' ? true : 'auto',
+        // sameSite: "none",
+        sameSite: process.env.NODE_ENV === 'PROD' ? 'none' : 'lax',
+        domain: process.env.FRONTEND_URL,
     }
     
     res.status(statuscode).cookie("token", token, options).json({
